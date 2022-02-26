bc1ee03b8d3d58d06386e023bc15d4b91c413def
LyubimtsevSemyon <99214966+LyubimtsevSemyon@users.noreply.github.com>
2022-02-26 16:33:00 +0300

    Update alg.cpp

--- src/alg.cpp
+++ src/alg.cpp
@@ -2,59 +2,60 @@
 #include <cstdint>
 #include "alg.h"
 
+
 bool checkPrime(uint64_t value) {
 // вставьте код функции
-      if(value <= 1)
-        return 0;
-
-     for (uint64_t i = 2 ; i <= sqrt(value) ; i++)
-          if (value % i == 0)
-            return false;
-
-     return true;
+bool a = true;
+    for (int i = 1; i < value; i++) {
+        if ((value % i == 0) && (i != 1)) {
+            a = false;
+            break; }
+    }
+    return (a);
 }
 
 uint64_t nPrime(uint64_t n) {
 // вставьте код функции
-   uint64_t count = 0;
-    uint64_t i = 0;
-    for (uint64_t j = 2 ; j <= LONG_MAX ; j++) {
-        if (checkPrime(j)) {
-            i += 1;
-            if (i == n) {
-                count = j;
-                break;
-
-            }
-
+int m = 1;
+    int y = 0;
+    int i = 2;
+    while (y < n) {
+        bool a = true;
+        for (int sr = 2; sr < i; sr++) {
+            if (i % sr == 0) {
+                a = false;
+                break; }
         }
-
-    }
-return count;
+        if (a) {
+            m = i;
+            y++; }
+        i++; }
+    return(m);
 }
 
 uint64_t nextPrime(uint64_t value) {
 // вставьте код функции
-   uint64_t count=0;
-    uint64_t i=0;
-     for (uint64_t k = value + 1 ; k <= LONG_MAX ; k++) {
-        if (checkPrime(k)) {
-            count = k;
-            break;
+int sr = value;
+    while (true) {
+        bool a = true;
+        sr++;
+        for (int i = 2; i < sr; i++) {
+            if (sr % i == 0) {
+                a = false;
+                break; }
         }
+        if (a) {
+            return(sr);
+            break; }
     }
-    return count;
 }
 
 uint64_t sumPrime(uint64_t hbound) {
 // вставьте код функции
-      uint64_t sum=0;
-    for (uint64_t m = 2 ; m < hbound ; m++) {
-        if (checkPrime(m)) {
-             {
-                sum += m;
-            }
-        }
-    }
-    return sum;
+uint64_t sum = 0;
+  for (uint64_t  i = 2; i < hbound; i++) {
+    if (checkPrime(i)) {
+      sum += i; }
+  }
+  return sum;
 }
